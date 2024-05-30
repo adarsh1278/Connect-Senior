cbf09ad94325c95fbf897df93134fdcbc2608230
adarsh1278 <120383844+adarsh1278@users.noreply.github.com>
2024-05-30 16:15:08 +0530

    eroor solving

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,9 +1,9 @@
-import DrawerDialogDemo from "@/app/reply/page";
+
 export default function(){
     return (
         <div>
             hi
-            <DrawerDialogDemo />
+         
         </div>
     )
 }
