f76a68b9d90cf5908f7b927086510f4be9dba8fd
adarsh1278 <120383844+adarsh1278@users.noreply.github.com>
2024-05-31 19:25:35 +0530

    vercel error solving2

--- src/app/api/users/myDoubt/route.ts
+++ src/app/api/users/myDoubt/route.ts
@@ -6,51 +6,42 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     try {
         await dbConnect();
-        console.log("inside my doubt")
-        const token = await req.cookies.get('token')?.value || '';
+        console.log("inside my doubt");
+
+        const token = req.cookies.get('token')?.value || '';
         if (!token) {
             return NextResponse.json({
-                status: 404,
-                body: {
-                    message: "Token authentication failed",
-                    success: false,
-                },
-            });
+                message: "Token authentication failed",
+                success: false,
+            }, { status: 404 });
         }
 
         // Decode token
         const decodedToken: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret);
-
+        
         if (!decodedToken._id) {
             return NextResponse.json({
-                status: 404,
-                body: {
-                    message: "Token authentication failed",
-                    success: false,
-                },
-            });
+                message: "Token authentication failed",
+                success: false,
+            }, { status: 404 });
         }
 
         const doubts = await DoubtModel.find({ userId: decodedToken._id });
-  console.log(doubts)
+        console.log(doubts);
+
         return NextResponse.json({
-            status: 200,
-            body: {
-                message: "Doubts fetched",
-                success: true,
-                doubts,
-            },
-        });
+            message: "Doubts fetched",
+            success: true,
+            doubts,
+        }, { status: 200 });
     } catch (error) {
         console.error(error);
+
         return NextResponse.json({
-            status: 500,
-            body: {
-                message: "Something went wrong",
-                success: false,
-                doubts: "No doubts available",
-                error:error,
-            },
-        });
+            message: "Something went wrong",
+            success: false,
+            doubts: "No doubts available",
+            error: error,
+        }, { status: 500 });
     }
 }
